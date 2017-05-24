83bdaa7c7022ad69d58ee9192e46329f7d54e7da
nativanando <nativanando@gmail.com>
2017-05-23 21:46:37 -0300

    modificando treinamento do MLP por uma base real

--- model/FeedForward.py
+++ model/FeedForward.py
@@ -3,6 +3,7 @@ from pybrain.structure import LinearLayer, SigmoidLayer
 from pybrain.structure import FullConnection
 from pybrain.datasets import SupervisedDataSet
 from pybrain.supervised import BackpropTrainer
+import timeit
 
 __author__ = 'Fernando Natividade.'
 
@@ -26,9 +27,9 @@ class FeedFoward:
         self.defineArquitetura()
 
     def defineArquitetura(self):
-        self.camada_entrada = LinearLayer(self.camada_entrada, name="in")
-        self.camada_oculta = SigmoidLayer(self.camada_oculta, name="hidden")
-        self.camada_saida = LinearLayer(self.camada_saida, name="out")
+        self.camada_entrada = LinearLayer(self.camada_entrada, name="entrada")
+        self.camada_oculta = SigmoidLayer(self.camada_oculta, name="oculta")
+        self.camada_saida = LinearLayer(self.camada_saida, name="saida")
         self.adicionaEstrutura()
 
     def adicionaEstrutura(self):
@@ -46,7 +47,7 @@ class FeedFoward:
 
     def visualizaPesosSinapticos(self):
         print('peso camada_entrada_oculta',
-              self.ligacao_entrada_oculta.params)  # mostra os pesos das conexões de entrada para camada oculta, todas interligadas entre sí. 2x3 = 6 pesos sinpaticos
+              self.ligacao_entrada_oculta.params)  # mostra os pesos das conexões de entrada para camada oculta, todas interligadas entre sí. 3x4 = 12 pesos sinpaticos
         print('peso camada_oculta_saida',
               self.ligacao_oculta_saida.params)  # mostra os pesos das conexões da camada oculta para a camada de saída. Todas interligadas entre sí. 3x1 = 3 pesos sinapticos
         print('pesos rede', self.network.params)
@@ -54,29 +55,37 @@ class FeedFoward:
     def iniciaRede(self):
         self.network.sortModules()
 
+#1-doente 0-saudável     treino S-1 N-0        Peq-1 grand-0
+ref_arquivo = open("dados.txt","r")
+dataset = SupervisedDataSet(8, 1)
 
-dataset = SupervisedDataSet(3, 1)
-dataset.addSample([1, 1, 1], [1])
-dataset.addSample([1, 0, 0], [0])
-dataset.addSample([0, 1, 1], [1])
-dataset.addSample([0, 0, 1], [0])
-dataset.addSample([0, 0, 0], [0])
+for linha in ref_arquivo:
+    valores = linha.split()
+    vetor = (valores[0].split(","))
+    dataset.addSample([ vetor[0], vetor[1], vetor[2], vetor[3], vetor[4], vetor[5], vetor[6], vetor[7] ], [vetor[8]])
+
 
 network = None
-rna = FeedFoward(network, 3, 4, 1)
+rna = FeedFoward(network, 8, 16, 1)
 
 trainer = BackpropTrainer(rna.network, dataset, verbose=True, learningrate=0.01, momentum=0.99)
-for epoch in range(0, 10000):  # treina por 1000 iterações para ajuste de pesos
+start = timeit.default_timer()
+
+for epoch in range(0, 100):  # treina por 1000 iterações para ajuste de pesos
     resultTrainer = trainer.train()
 
+stop = timeit.default_timer()
 print(resultTrainer)
-
 rna.visualizaPesosSinapticos()
+print ("tempo de execução", stop - start)
 
-test_data = SupervisedDataSet(3, 1)
-test_data.addSample([1, 1, 0], [1])
-test_data.addSample([1, 1, 0], [1])
-test_data.addSample([1, 0, 0], [0])
-test_data.addSample([0, 1, 1], [1])
-result = trainer.testOnData(test_data, verbose=True)  # verbose=True indica que deve ser impressas mensagens
-print(result)
+##test_data = SupervisedDataSet(4, 1)
+##test_data.addSample([1, 1, 0, 1], [0])
+##test_data.addSample([1, 1, 0, 1], [0])
+##test_data.addSample([0, 0, 1, 1], [0])
+##test_data.addSample([0, 0, 1, 1], [0])
+
+##result = trainer.testOnData(test_data, verbose=True)  # verbose=True indica que deve ser impressas mensagens
+##erroMedio = result
+##print ("erro medio encontrado no teste", erroMedio)
+
